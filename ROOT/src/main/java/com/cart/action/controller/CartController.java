1cfad2b754232606fe9855bf5f3e0e1a6a388267
i3inni <fkdl4862@a.ut.ac.kr>
2025-10-15 20:51:22 +0900

    카트 api 완료

--- ROOT/src/main/java/com/cart/action/controller/CartController.java
+++ ROOT/src/main/java/com/cart/action/controller/CartController.java
@@ -1,9 +1,8 @@
 package com.cart.action.controller;
-
-import java.io.IOException; 
+ 
+import java.io.IOException;  
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-// import javax.servlet.annotation.WebServlet; // 어노테이션을 사용하지 않으므로 이 import 문은 제거합니다.
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,6 @@ import com.ActionForward;
 import com.cart.action.CartAddAction;
 import com.cart.action.CartPageAction;
 
-// @WebServlet("/myMenu/*") // web.xml에서 매핑을 처리하기 위해 어노테이션을 제거합니다.
 public class CartController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -26,21 +24,29 @@ public class CartController extends HttpServlet {
 
     protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
+        // response.setCharacterEncoding("UTF-8"); // Action 클래스에서 직접 처리하는 경우가 많으므로 생략 가능
 
-        String command = request.getRequestURI();
+        // [수정] 1. ProductController와 동일하게 가상주소 계산
+        String requestURI = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        String command = requestURI.substring(contextPath.length());
+        
+        System.out.println("C: CartController가 받은 command: " + command); // 디버깅용 로그
+
         Action action = null;
         ActionForward forward = null;
 
         try {
             // 요청별로 Action 분기
+            // [수정] 2. 올바른 URL 경로로 조건문 변경
             if (command.equals("/cart/cartPage.do")) {
-            	action = new CartPageAction();
+                action = new CartPageAction();
                 forward = action.execute(request, response);
             } else if (command.equals("/cart/cartAddAction.do")) {
-            	action = new CartAddAction();
+                action = new CartAddAction();
                 forward = action.execute(request, response);
             }
+            
             // 이동 처리
             if (forward != null) {
                 if (forward.isRedirect()) {
@@ -49,10 +55,13 @@ public class CartController extends HttpServlet {
                     RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
                     dispatcher.forward(request, response);
                 }
+            } else {
+                 System.out.println("C: 이동할 페이지 정보(ActionForward)가 없습니다."); // 디버깅용 로그
             }
 
         } catch (Exception e) {
             e.printStackTrace();
+            // 필요하다면 이곳에 에러 페이지로 이동하는 로직을 추가할 수 있습니다.
         }
     }
 }
